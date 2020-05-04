d9e14b9bca96b2f5b68748a6046475191e7d8ca0
dandanutz66 <dandanutz66@yahoo.com>
2020-05-04 19:00:41 +0300

    Prima tema

---
+++ Teme/Daniel/L3/js/app.js
@@ -0,0 +1,15 @@
+function primafunctie() {
+    alert("eu sunt prima functie! Tu cine esti?!")
+}
+
+///acesta este un comentariu in sj single line
+/*acesta este un comentariu
+in js
+multi line*/
+///function este 'cel care anunta' ca de aici incepe declararea unei functii
+///primul element dupa 'function' reprezinta numele functiei
+function Adouafuntie(/*intre parantezele rotunde se pun parametrii*/)  
+{
+
+    ///intre acolade se pune continutul(liniile de cod)
+}
