608f1a90886973148eb12f8982941da072c1ee66
dragangabriel40 <dragangabriel40@yahoo.comm>
2020-05-07 23:56:44 +0300

    Tema pentru L3

---
+++ Teme/Gabi/L4/js/app.js
@@ -0,0 +1,20 @@
+var nume = "Firma"
+
+
+Nume = "LEMET"  // nume companie
+        alert("Acesta este: " + nume)
+ 
+nume = "Brebu"  // nume localitate
+        alert("Localitatea este "+ nume)
+
+nume = "Roomania" // tara
+        alert ("Tara de unde provine este " + nume)
+
+nume = "Gabi" // nume administrator
+        alert ("Numele administratorului este " + nume)
+
+nume = "Campina"   // localitate schimbata
+        alert ("Nume localitate schimbata " + nume)
+
+nume= Dragan       // nume administrator schimbat
+         alert ("Nume administrator schimbat " + nume )
