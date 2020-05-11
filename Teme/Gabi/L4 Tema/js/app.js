8f1ab290bba817bae02355004d58c2347331abe0
dragangabriel40 <dragangabriel40@yahoo.comm>
2020-05-11 11:23:37 +0300

    Am rezolvat tema L4

---
+++ Teme/Gabi/L4 Tema/js/app.js
@@ -0,0 +1,58 @@
+var DenumireCurs
+    DenumireCurs = "InvatamProgramare"
+    console.log("Denumirea cursului " + DenumireCurs)
+
+var NumeTrainer 
+    NumeTrainer = "Ovidiu Rudi"     
+    console.log("Numele trainerului este " + NumeTrainer)
+
+var NumarParticipanti 
+    NumarParticipanti = 5
+    console.log("Numarul de participanti este de "+ NumarParticipanti)
+
+var Durata
+    Durata = "2 ore"
+    console.log("Durata curs este de "+ Durata)
+
+var EsteOnline 
+    EsteOnline = true
+    console.log("Curse se desfasoara online " + EsteOnline)
+    
+var Locatie 
+    Locatie = "Acasa"
+    console.log("Locatia cursului este " + Locatie)
+
+var aInnebunit 
+    aInnebunit = true
+    console.log("Ai innebunit " + aInnebunit)
+
+var punctajCursant1
+    punctajCursant1 = 80
+
+var punctajCursant2
+    punctajCursant2 = 90
+
+var punctajCursant3
+    punctajCursant3 = 67
+
+var punctajCursant4
+    punctajCursant4 = 48
+    
+var punctajCursant5
+    punctajCursant5 = 21
+    
+
+var punctajCursant6
+    punctajCursant6 = 24
+    
+    
+var medie 
+    medie = punctajCursant1 + punctajCursant2 + punctajCursant3 + punctajCursant4 + punctajCursant5 + punctajCursant6 / 6
+  console.log("suma = " + medie)
+    
+var PunctajMic =Math.min (punctajCursant1, punctajCursant2, punctajCursant3, punctajCursant4, punctajCursant5, punctajCursant6)
+        console.log ("Cea mai mica medie este " + PunctajMic)
+
+var PunctajMare = Math.max (punctajCursant1, punctajCursant2, punctajCursant3, punctajCursant4, punctajCursant5, punctajCursant6)
+        console.log ("Cea mai mare medie este " + PunctajMare)
+        
