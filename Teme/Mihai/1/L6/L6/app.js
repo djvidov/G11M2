3cffc8eabfbe92286c6829e4cee2d281f1b130d2
mitone2 <mocanumiahi@yahoo.com>
2020-05-18 08:15:07 -0700

    tema mea

---
+++ Teme/Mihai/1/L6/L6/app.js
@@ -0,0 +1,26 @@
+function scadere(a, b){
+    var scadere=a-b
+    alert(`Scaderea numerelor ${a} si ${b} este ${scadere}`)
+    }
+    scadere(8, 4)
+    
+    function adunare(a, b, c){
+        var suma=a+b+c
+        alert(`Adunarea numerelor ${a} ,${c} si ${b} este ${suma}`)
+        }
+        adunare (8, 4,3)
+    
+        function impartire(a, b) {
+            var impartire= a / b
+            alert(`Suma numerelor ${a} si ${b} este ${impartire}`)
+        }
+        impartire(8,4)
+        
+        function inmultire(a,b,c) {
+            var inmultire =a*b*c
+            alert(`rezultatul inmultirii este ${inmultire} `)
+        }
+            inmultire(8,4,3)
+     
+        
+     
