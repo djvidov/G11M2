bba6317ee88f5b41dbbfa2c7aaa3ed60cda6f281
Your Name <you@example.com>
2020-05-18 18:10:58 +0300

    tema pentru laboratorul 4

---
+++ Teme/Cristi/L4/js/app.js
@@ -0,0 +1,76 @@
+alert("merge!")
+function denumireCurs()
+{
+var numegrup ="grupa de soc"
+console.log(numegrup)
+
+}
+function numeTrainer()
+{
+var numeTrainer ="Ovidiu Rudi"
+console.log(numeTrainer)
+
+}
+
+function numarParticipanti()
+{
+var numarParticipanti="6"
+console.log()
+
+}
+
+function durata()
+{
+var durata="5"
+
+
+}
+
+function esteOnline()
+{
+
+console.log("da")
+}
+
+function locatie()
+{
+var locatie ="Campina"
+log.console(locatie)
+
+}
+
+function aInebunit()
+{
+
+
+}
+
+function punctajCursant1()
+{
+
+
+}
+
+function punctajCursant2()
+{
+
+
+}
+
+function punctajCursant3()
+{
+
+
+}
+
+function punctajCursant4()
+{
+
+
+}
+
+function punctajCursant5()
+{
+
+
+}
