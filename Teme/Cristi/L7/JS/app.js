b23a7a76de3e0a7a809abc5c7ac6d0b450e8039d
Your Name <you@example.com>
2020-05-22 16:17:29 +0300

    tema pt L7

---
+++ Teme/Cristi/L7/JS/app.js
@@ -0,0 +1,104 @@
+//alert("neata")
+
+
+function inmultire(a, b, c) {
+    var inmultire = a * b * c
+    return inmultire
+
+}
+
+var pahar1 = 7
+
+pahar1 = inmultire(5, 2, 3)
+
+
+
+function suma(a, b, c) {
+    var suma = a + b + c
+    return suma
+
+}
+
+var pahar = 7
+
+pahar = suma(3, 4, pahar1)
+
+alert("prima operatie este egala cu " + pahar)
+
+
+
+
+function suma(a, b, c) {
+    var suma = a + b + c
+    return suma
+
+}
+
+var pahar = 7
+
+pahar = suma(3, 4, 5)
+
+
+function inmultire(a, b, c) {
+    var inmultire = a * b * c
+    return inmultire
+
+}
+
+var pahar1 = 7
+
+pahar1 = inmultire(pahar, 2, 3)
+
+alert("a doua operatie este egala cu " + pahar1)
+
+
+
+function inmultire(a, b, c) {
+    var inmultire = a * b * c
+    return inmultire
+
+}
+
+var pahar1 = 7
+
+pahar1 = inmultire(2, 2, 5)
+
+
+
+
+function scadere(a, b) {
+    var scadere = a - b
+    return scadere
+
+}
+
+var pahar2 = 7
+pahar2 = scadere(5, pahar1)
+
+alert("a treia operatie este egala cu " + pahar2)
+
+
+
+function scadere(a, b) {
+    var scadere = a - b
+    return scadere
+
+}
+
+var pahar2 = 7
+pahar2 = scadere(5, 2)
+
+
+function inmultire(a, b, c) {
+    var inmultire = a * b * c
+    return inmultire
+
+}
+
+var pahar1 = 7
+
+pahar1 = inmultire(pahar2, 2, 5)
+
+alert("a patra operatie este egala cu " + pahar1)
+
+
