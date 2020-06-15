bea00ad240836518e8a12cc2aa55d8088e2561eb
Your Name <you@example.com>
2020-06-15 20:03:04 +0300

    l8 cristi

---
+++ Teme/Cristi/L8/JS/app.js
@@ -0,0 +1,27 @@
+listajucatori.push("Hagi")
+listajucatori.push("Adi Mutu")
+listajucatori.push("Dan Petrescu")
+listajucatori.push("Dan Popescu")
+listajucatori.push("Florin Prunea")
+listajucatori.push("Ionut Moldovan")
+
+
+function numarPersLista1(listajucatori) {
+
+
+    for (i = 0; i >= listajucatori; i++) {
+
+
+
+
+
+    }
+    var numar = i
+
+
+    return numar
+
+}
+
+function numarPersLista1()
+alert(numar)
