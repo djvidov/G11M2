0c1cb4256055e700a03ffa2d22c92377bf26e071
mitone2 <mocanumiahi@yahoo.com>
2020-06-19 09:05:05 -0700

    am lucrat la laboratorul 11 part 1

---
+++ Teme/Mihai/Teme/L 11 2.0/js/app.js
@@ -0,0 +1,23 @@
+var tastaApasata = 4
+function afiseazaMesajul() {
+    debugger
+    if (tastaApasata == 1) {
+        console.log("Buna ziua")
+    }
+    else if (tastaApasata == 2) {
+        console.log("Good morning")
+    }
+    else if (tastaApasata == 3) {
+        console.log("Bonjour")
+    }
+    else if (tastaApasata == 4) {
+        console.log("germana")
+    }
+    else if (tastaApasata == 5) {
+        console.log("italiana")
+    }
+    else if (tastaApasata == 6) {
+        console.log("spaniola")
+    }
+}
+afiseazaMesajul()
