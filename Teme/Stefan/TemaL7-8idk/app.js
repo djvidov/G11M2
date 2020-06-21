88546c8fc4ccf454cdb8b31f4c565ce57e67d765
Stefanzzz123 <boziesan.stefan@gmail.com>
2020-06-21 12:46:56 +0300

    long gone tema; neinteresant

---
+++ Teme/Stefan/TemaL7-8idk/app.js
@@ -0,0 +1,42 @@
+var jucatoriFotbal = [
+    "Hagi",
+    "Adi Mutu",
+    "Dan Petrescu",
+    "Dan Popescu",
+    "Florin Prunea",
+    "Ionut Moldovan",
+    "Hagi",
+    "Adi Mutu",
+    "Dan Petrescu",
+    "Dan Popescu",
+    "Florin Prunea",
+    "Ionut Moldovan"
+]
+
+// 1. Creati o functie care numara jucatorii dintr-o lista (lista de jucatori va fi parametru)
+// 2. Creati o functie care afiseaza toti jucatorii dintr-o lista (lista de jucatori va fi parametru)
+// 3. Creati o functie care sorteaza in ordine crescatoare jucatorii dintr-o lista (lista de jucatori va fi parametru) va returna o lista sortata
+// 4. folositi cele 3 functiii pentru ambele liste de jucatori 
+
+// 1
+function numaraJucatori(a) {
+    for (var i = 0; i < a.length; i++) {
+
+    }
+    return i
+}
+var nrJucatoriFotbal = numaraJucatori(jucatoriFotbal)
+console.log(nrJucatoriFotbal)
+
+//2 
+function afiseazaJucatori(a) {
+    for (var i = 0; i < a.length; i++) {
+        console.log(a[i])
+    }
+}
+var jucatori = afiseazaJucatori(jucatoriFotbal)
+
+//3
+function sorteazaLista(a) {
+    vsvasdfafawfawf
+}
