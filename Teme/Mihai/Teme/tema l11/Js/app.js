b3ca76c3e516a522cbb5706a771540bff4ee3528
mitone2 <mocanumiahi@yahoo.com>
2020-06-22 07:26:56 -0700

    Tema

---
+++ Teme/Mihai/Teme/tema l11/Js/app.js
@@ -0,0 +1,36 @@
+var tastaApasata = "L"
+function actiuneJucator() {
+    switch (tastaApasata) {
+        case "W":
+            console.log("Ai mers in fata")
+            break;
+        case "A":
+            console.log("Ai mers in stanga")
+            break;
+        case "S":
+            console.log("Ai mers in spate")
+            break;
+        case "D":
+            console.log("Ai mers in dreapta")
+            break;
+        case "L":
+            console.log("Ai activat ultimata:Butoiul cu Bere")
+            break;
+    }
+}
+actiuneJucator()
+//evaluarea scorului
+function evaluareaScorului(temperaturaAerului) {
+    if (temperaturaAerului < 5) {
+        console.log("NU BEI!")
+    }
+    else if (temperaturaAerului < 15) {
+        console.log("NU AI BANI")
+    }
+    else {
+        console.log("POTI SA BEI!")
+    }
+}
+evaluareaScorului(3)
+evaluareaScorului(7)
+evaluareaScorului(16)
