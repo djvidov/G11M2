a520badbebf209a353a025e71aca963fa065c997
mitone2 <mocanumiahi@yahoo.com>
2020-06-22 07:27:39 -0700

    m

---
+++ Teme/Mihai/Teme/L11/app.js/app.js
@@ -0,0 +1,24 @@
+var listaNumere = [
+    1,
+    2,
+    3,
+    4,
+    5
+]
+//afisati suma numerelor
+index = 0
+function adunaNumereleDinLista(listaNumere) {
+    for (let index = 0; index < 2; index++) {
+
+        var suma = listaNumere[index] + listaNumere[index + 1]
+        var temp = suma
+    
+    }
+}
+
+
+
+
+
+
+console.log(adunaNumereleDinLista(listaNumere))
