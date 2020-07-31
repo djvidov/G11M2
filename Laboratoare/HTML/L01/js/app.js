7e3ae61b083ade7823b073a9693385d7ab763ac7
Ovidiu Rudi <djvidov@yahoo.com>
2020-07-31 19:59:01 +0300

    am lucrat la laboratorul 01

---
+++ Laboratoare/HTML/L01/js/app.js
@@ -0,0 +1,15 @@
+function makeNight() {
+    var page = document.getElementsByTagName('body')[0];
+    if (page != null) {
+        page.classList.remove('day')
+        page.classList.add('night')
+    }
+}
+
+function makeDay() {
+    var page = document.getElementsByTagName('body')[0];
+    if (page != null) {
+        page.classList.remove('night')
+        page.classList.add('day')
+    }
+}
